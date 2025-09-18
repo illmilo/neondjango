7752abe72cc578eaa607c0c8eeb3857315da73f6
illmilo <illfqm@gmail.com>
2025-09-18 17:42:10 +0300

    Removed dependency on external style. Minor HTML changes

--- main/static/js/readmeParser.js
+++ main/static/js/readmeParser.js
@@ -2,6 +2,7 @@ fetch('https://raw.githubusercontent.com/illmilo/neondjango/master/readme.md')
     .then(response => response.text())
     .then(markdown => {
       // Use a Markdown parser like marked.js
-      document.getElementById('readme-container').innerHTML = 
-        marked.parse(markdown);
+      const containers = document.getElementsByClassName('readme-container');
+      for (let i = 0; i < containers.length; i++)
+        containers.item(i).innerHTML = marked.parse(markdown);
     });
